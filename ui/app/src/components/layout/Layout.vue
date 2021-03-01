232793fd09adf299b9bf654806d57442bc069db0
59023g <5235538+59023g@users.noreply.github.com>
2021-03-01 10:39:01 -0800

    app/Layout: sketch 2nd panel

--- ui/app/src/components/layout/Layout.vue
+++ ui/app/src/components/layout/Layout.vue
@@ -24,6 +24,94 @@
     <Panel v-if="!!$slots.after" class="after">
       <slot name="after"></slot>
     </Panel>
+    <Panel class="secondary">
+      <div class="secondary-conteainer header dark">
+        Notifications
+      </div>
+      <div class="secondary-container">
+        Sif Network Dispatch: 
+                <ul>
+          <li>
+        - <a href="">Network congestion due to high volume. Expect high gas for the next 12 hours ></a>
+
+        </li>
+        <li>
+        - <a href="">Network updated! See 1.23.1 release notes ></a>
+        </li>
+                <li>
+        - <span>Rowan reaches all-time high. $14!</span>
+        </li>
+        <li style="text-align: center">
+          <span style="color: #8cc63f; margin-left: 140px"> [ CHART ] </span>
+          </li>
+          </ul>
+      </div>
+      <div class="secondary-container grey">
+        <span class="header">Balances</span>
+        <hr />
+        <ul>
+          <li><span>Connected ETH Wallet:</span>
+          <a href="">0xc3b265.........E305b000e7D</a><span style="margin-left: 8px"><!--v-if--><!--v-if--><!--v-if--><!--v-if--><svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12"><g id="Gruppe_1223" data-name="Gruppe 1223" transform="translate(0.246 0.131)"><circle id="Ellipse_29" data-name="Ellipse 29" cx="6" cy="6" r="6" transform="translate(-0.246 -0.131)" fill="#8cc63f"></circle><path id="Pfad_1915" data-name="Pfad 1915" d="M1253.022,450.614l1.955,1.955,3.678-3.678" transform="translate(-1249.998 -444.861)" fill="none" stroke="#fff" stroke-linecap="round" stroke-miterlimit="10" stroke-width="1.5"></path></g></svg><!--v-if--><!--v-if--><!--v-if--><!--v-if--><!--v-if--></span></li>
+          <!-- <hr> -->
+          <li>
+            <span>ETH</span>
+            <a href="">33.344</a>
+          </li>
+          <li>
+            <span>USDT</span>
+            <a href="">9774.12</a>
+          </li>
+          <hr>
+          <li style="margin-top: 8px"><a href="">Connect Sif Wallet ></a></li>
+          <!-- <li>
+            <span>Rowan</span>
+            <a href="">481233.1222</a>
+          </li>
+          <li>
+            <span>cUSDT</span>
+            <a href="">12392.2</a>
+          </li> -->
+        </ul>
+      </div>
+      <div class="secondary-container">
+        <span class="header">Pending Transactions</span>
+        <hr />
+        <ul>
+          <li>
+            <span>Peg: ETH -> cETH</span>
+            <a href="">0x8e18317f...b9c253db3d1b054</a>
+          </li>
+          <li>
+            <span>Peg: eRowan -> Rowan</span>
+            <a href="">0x2kk3faxs...b9c253db3d1b054</a>
+          </li>
+          <li>
+            <span>Unpeg: cETH -> ETH</span>
+            <a href="">0x8e18317f...b9c253db3d1b054</a>
+          </li>
+        </ul>
+      </div>
+
+
+      <div class="secondary-container grey">
+        <span class="header">History</span>
+        <hr />
+        <ul>
+          <li>
+            <span>Swap: cUNI -> cETH</span>
+            <a href="">34E1E......97E89B5D2BCF6244F</a>
+          </li>
+          <li>
+            <span>Add Liq: cUSDT/Rowan</span>
+            <a href="">21kdE......97E89B5D2BCF6244F</a>
+          </li>
+          <li>
+            <span>Unpeg: cETH -> ETH</span>
+            <a href="">0x8e18317f...b9c253db3d1b054</a>
+          </li>
+        </ul>
+      </div>
+    </Panel>
   </div>
 </template>
 
@@ -47,6 +135,31 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
+.header.dark {
+  background: #474747;
+  color: white;
+  padding: 16px;
+  margin-bottom: 0px;
+  font-size: 18px
+}
+.secondary-container {
+  .header{ font-size: 16px; margin-bottom: 4px}
+  padding: 16px;
+  text-align: left;
+  &.grey {
+    background: #f0f0f0;
+  }
+  ul {
+    margin-top: 8px;
+    li {
+      display: flex;
+      flex-direction: row;
+      span {
+        margin-right: 8px;
+      }
+    }
+  }
+}
 .layout {
   background: url("../../assets/World_Background_opt.jpg");
   background-size: cover;
@@ -57,8 +170,9 @@ export default defineComponent({
   height: 100vh; /* TODO: header height */
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  align-items: center;
+  justify-content: space-evenly;
+  align-items: end;
+  flex-direction: row;
 }
 
 .after {
